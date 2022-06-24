806ed8cb9f13fde193f824208d8cea9b9f37f3a0
Remus Simion <39557731+SimionRemus@users.noreply.github.com>
2022-06-24 11:15:14 +0300

    Added change of state based on keypresses
    Pressing Z,X,C,V,B,N changes state to "LoadData", "IntroCine", "Settings", "Encyclop", "Credits" and quit application respectively.

--- Assets/Scripts/GSM/SCR_MainMenuState.cs
+++ Assets/Scripts/GSM/SCR_MainMenuState.cs
@@ -28,6 +28,36 @@ public class SCR_MainMenuStates : IState
 
     void IState.OnUpdate()
     {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            // Change state LoadDataState (Continue Game)
+            stateMachine.ChangeState(stateMachine.LoadDataState)
+        }
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            // Change state to Intro Cinematic (New Game)
+            stateMachine.ChangeState(stateMachine.IntroCineState)
+        }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            //Change state to Settings
+            stateMachine.ChangeState(stateMachine.SettingsState)
+        }
+        if(Input.GetKeyDown(KeyCode.V))
+        {   
+            // Change state to Encyclopedia
+            stateMachine.ChangeState(stateMachine.EncyclopediaState)
+        }
+        if(Input.GetKeyDown(KeyCode.B))
+        {   
+            // Change state to Show Credits
+            stateMachine.ChangeState(stateMachine.ShowCreditsState)
+        }
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            Application.Quit();
+            Debug.Log("Quitting the game has no effect in the Unity Editor so i'm giving this console message also.");
+        }
         Debug.Log("Showing Main menu here");
     }
 }
