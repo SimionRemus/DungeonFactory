f806f4c6efd8f876e748ce11c3464aa65ec2e3d2
Remus Simion <39557731+SimionRemus@users.noreply.github.com>
2022-06-24 11:21:56 +0300

    Added return in case state is changed

--- Assets/Scripts/GSM/SCR_MainMenuState.cs
+++ Assets/Scripts/GSM/SCR_MainMenuState.cs
@@ -33,31 +33,37 @@ public class SCR_MainMenuStates : IState
         {
             // Change state LoadDataState (Continue Game)
             stateMachine.ChangeState(stateMachine.LoadDataState)
+            return;
         }
         if(Input.GetKeyDown(KeyCode.X))
         {
             // Change state to Intro Cinematic (New Game)
             stateMachine.ChangeState(stateMachine.IntroCineState)
+            return;
         }
         if(Input.GetKeyDown(KeyCode.C))
         {
             //Change state to Settings
             stateMachine.ChangeState(stateMachine.SettingsState)
+            return;
         }
         if(Input.GetKeyDown(KeyCode.V))
         {   
             // Change state to Encyclopedia
             stateMachine.ChangeState(stateMachine.EncyclopediaState)
+            return;
         }
         if(Input.GetKeyDown(KeyCode.B))
         {   
             // Change state to Show Credits
             stateMachine.ChangeState(stateMachine.ShowCreditsState)
+            return;
         }
         if(Input.GetKeyDown(KeyCode.N))
         {
             Application.Quit();
             Debug.Log("Quitting the game has no effect in the Unity Editor so i'm giving this console message also.");
+            return;
         }
         Debug.Log("Showing Main menu here");
     }
