9bae44685dcacfdcdbb66a5eef310d9a392a2415
Remus Simion <remusasimion@gmail.com>
2022-07-10 16:05:03 +0300

    Clicked tile changes tint if SC selected. Console tells SC and tile
    Clicked tile changes tint if spellcard selected. Console tells spellcard and tile

--- Assets/Scripts/GSM/SCR_PlayerTurnIdleState.cs
+++ Assets/Scripts/GSM/SCR_PlayerTurnIdleState.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Tilemaps;
 
 public class SCR_PlayerTurnIdleState : IState
@@ -9,6 +10,7 @@ public class SCR_PlayerTurnIdleState : IState
     private SCR_GameSM stateMachine;
     private Tilemap groundTilemap;
     private Tilemap wallTilemap;
+    private GameObject selectedCard;
 
     /// <summary>
     /// Constructor of state. Passes needed parameters into the state.
@@ -50,23 +52,43 @@ public class SCR_PlayerTurnIdleState : IState
             stateMachine.ChangeState(stateMachine.MainMenuState);
             return;
         }
-        if (Input.GetMouseButtonDown(0))
+        var cardSelectionBox = stateMachine.GameUI.transform.Find("CardSelectionBox").GetComponent<Image>();
+        if (cardSelectionBox.enabled) 
         {
-            Vector3 pos = groundTilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-            //Debug.Log(string.Format("Co-ords of mouse is [X: {0} Y: {1}]", pos.x, pos.y));
-            TileWithAttributes tileground = (TileWithAttributes)groundTilemap.GetTile(new Vector3Int((int)pos.x, (int)pos.y, 0));
-            TileWithAttributes tilewall = (TileWithAttributes)wallTilemap.GetTile(new Vector3Int((int)pos.x, (int)pos.y, 0));
-            if (tileground != null)
+            GameObject actionlist = stateMachine.GameUI.transform.Find("ActionList").gameObject;
+            int cardnumber = stateMachine.GameUI.transform.Find("ActionList").childCount;
+            for (int i = 0; i < cardnumber; i++)
             {
-                //Detect what kind of tile it is.
-                Debug.Log(string.Format("Tile is: {0}", tileground.tileName));
-                //Check if anything is on tile:
-                Vector3 midpoint=groundTilemap.CellToWorld(new Vector3Int((int)pos.x, (int)pos.y, 0))+new Vector3(0.5f,0.5f,0);
-                Collider2D collider = Physics2D.OverlapCircle(midpoint, 0.45f);
-                if (collider)
+                var currentCard=actionlist.transform.GetChild(i);
+                if (currentCard.position==cardSelectionBox.rectTransform.position)
                 {
-                        Debug.Log(collider.gameObject.name);
+                    selectedCard = currentCard.gameObject;
                 }
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 pos = groundTilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                //Debug.Log(string.Format("Co-ords of mouse is [X: {0} Y: {1}]", pos.x, pos.y));
+                Vector3Int tilePos = new Vector3Int((int)pos.x, (int)pos.y, 0);
+                TileWithAttributes tileground = (TileWithAttributes)groundTilemap.GetTile(tilePos);
+                if (tileground != null)
+                {
+                    //Detect what kind of tile it is.
+                    Debug.Log(string.Format("Casting spell {0} on tile {1}.",selectedCard.GetComponent<SCR_CardInfoDisplay>().SpellCard.name ,tileground.tileName));
+                    //Set clicked tile to red tint
+                    groundTilemap.SetTileFlags(tilePos, TileFlags.None);
+                    groundTilemap.SetColor(tilePos, new Color32(100, 100, 255, 255));
+                    //Check if anything is on tile:
+                    Vector3 midpoint = groundTilemap.CellToWorld(new Vector3Int((int)pos.x, (int)pos.y, 0)) + new Vector3(0.5f, 0.5f, 0);
+                    Collider2D collider = Physics2D.OverlapCircle(midpoint, 0.45f);
+                    if (collider)
+                    {
+                        Debug.Log(collider.gameObject.name);
+                    }
+                }
+            }
+            else
+            {
                 //Change tile with another tile (example changes to Life.
                 //groundTilemap.SetTile(new Vector3Int((int)pos.x, (int)pos.y, 0), stateMachine.grid.GetComponent<SCR_FloorGeneration>().tilePrefabs[6]);
             }
